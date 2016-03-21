9758daf40dd4c9bdb7f28d64c34369484aaf78b0
ChenLi@Chipsea <chencassc@gmail.com>
2016-03-21 15:55:36 +0800

    release 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cassc/clj-props "0.1.1"
+(defproject cassc/clj-props "0.1.2"
   :description "Clojure library for loading edn configuration file"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
